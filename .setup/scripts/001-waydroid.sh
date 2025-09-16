208e903003bf0d9fcb606aa742639c18c8b45a12
neilgfoster <1370457+neilgfoster@users.noreply.github.com>
2025-09-16 22:00:44 +0100

    Refactor Waydroid installation script to improve dependency installation formatting

--- .setup/scripts/001-waydroid.sh
+++ .setup/scripts/001-waydroid.sh
@@ -16,11 +16,22 @@ if [[ $install_waydroid =~ ^[Yy]$ ]]; then
   # Install build dependencies
   sudo apt update  -y
   sudo apt install -y \
-    build-essential
-    bc flex bison libelf-dev git software-properties-common lsb-release
+    curl \
+    build-essential \
+    bc \
+    flex \
+    bison \
+    libelf-dev \
+    git \
+    software-properties-common \
+    lsb-release
 
   # Install LLVM tools
   LLVM_VERSION=18
+  curl -L https://apt.llvm.org/llvm.sh | sudo bash -s "${LLVM_VERSION}"
+  for f in clang clang++ ld.lld llvm-objdump llvm-ar llvm-nm llvm-strip llvm-objcopy llvm-readelf; do
+    sudo ln -sf $f-$LLVM_VERSION /usr/bin/$f
+  done
 
 
 fi
